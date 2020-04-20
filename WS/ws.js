3665e24b4d486ade8f5e6d509135e27171dafb0b
fabio-sist-sdg <55802391+fabio-sist-sdg@users.noreply.github.com>
2020-04-20 09:13:12 +0200

    Update ws.js

--- WS/ws.js
+++ WS/ws.js
@@ -18,12 +18,17 @@
 				this.$URL = changedProperties["URL"];
 			}
 			if (this.$URL != ''){
-				let response = fetch(this.$URL);
-				console.log(response.status);
-				// get the response body (the method explained below)
-				let json = response.json();
+				fetch(this.$URL)
+				.then( function(response) {
+					console.log(response.status); // Will show you the status
+					changedProperties["response_code"] = response.status;
+				if (!response.ok) {
+					throw new Error("HTTP status " + response.status);
+				}
+				console.log(response.json());
 				changedProperties["response_body"] = json;
-				changedProperties["response_code"] = response.status;
+				return response.json();
+			});
 			}else{
 				console.log('NO WS');
 			}
